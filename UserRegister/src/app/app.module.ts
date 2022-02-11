c1efda13b428c1d7981832cf876de1aa314b8cf1
Letjobana <suzanm@gerbertech.co.za>
2022-02-11 15:10:31 +0200

    Added a registration form for user registration

--- UserRegister/src/app/app.module.ts
+++ UserRegister/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 
@@ -10,7 +10,7 @@ import { SignUpComponent } from './sign-up/sign-up.component';
     SignUpComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule,ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- UserRegister/src/app/sign-up/sign-up.component.ts
+++ UserRegister/src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
+import { User } from './Shared/user.model';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
+  user!: User;
   constructor() { }
 
   ngOnInit(): void {
+  }
+  resetForm(form:NgForm){
+    form.reset();
+
+    this.user={
+      UserName:'',
+      Password:'',
+      Email:'',
+      FirstName:'',
+      LastName:''
+    }
+
   }
 
 }
